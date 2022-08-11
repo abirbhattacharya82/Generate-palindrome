83e2707141920a3df394ad7ca6e0031698ba3d2f
abirbhattacharya82 <abirbhattacharya82@gmail.com>
2022-08-11 08:36:11 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,29 @@
+function isPalindrome(z)
+{
+    var x = z.toLowerCase();
+    var y = x.split('');
+    var z = y.reverse();
+    var a = z.join('');
+    if (x == a)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+function generatePalindrome(s)
+{
+    if(isPalindrome(s))
+    {
+        return s;
+    }
+    else
+    {
+        var x = s.toLowerCase();
+        var y = x.reverse();
+        var z = s+y;
+        return z;
+    }
+}
