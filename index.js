f45ce1deedddced39cdb2e6d0aaaa36e58c03276
abirbhattacharya82 <abirbhattacharya82@gmail.com>
2022-08-11 09:25:51 +0530

    Bugs

--- index.js
+++ index.js
@@ -1,36 +1,12 @@
-function isPalindrome(z)
-{
-    var x = z.toLowerCase();
-    var y = x.split('');
-    var z = y.reverse();
-    var a = z.join('');
-    if (x == a)
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
-}
-function generatePalindrome(s)
-{
-    if(isPalindrome(s))
-    {
+function generatePalindrome(s) {
+    var x = s.reverse();
+    if (s == x) {
         return s;
     }
     else
     {
-        var x = s.toLowerCase();
-        var y = x.reverse();
-        var z = s+y;
+        var z=s+x;
         return z;
     }
 }
-function main()
-{
-    console.log("Home");
-}
-module.exports = generatePalindrome;
-module.exports = isPalindrome;
-module.exports = main;
+export default generatePalindrome;
